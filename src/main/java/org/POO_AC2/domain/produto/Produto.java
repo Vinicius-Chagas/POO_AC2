1fbabd751c808a15965b951142131ba3e49864e1
Tales <tales.a.s.furlan@gmail.com>
2023-10-28 15:25:03 -0300

    começando classe produto

---
+++ src/main/java/org/POO_AC2/domain/produto/Produto.java
@@ -0,0 +1,12 @@
+package org.POO_AC2.domain.produto;
+
+public class Produto {
+    private Long codigo;
+    private String nomeProduto;
+    private String descricao;
+    private double preco;
+
+    public Long getCodigo(){
+        return codigo;
+    }
+}
