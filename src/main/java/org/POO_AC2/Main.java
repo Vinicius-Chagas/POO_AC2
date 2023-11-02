eafac710074e1984625fd32e562fbc46bb0f924b
Tales <tales.a.s.furlan@gmail.com>
2023-11-02 11:09:51 -0300

    switch case added

--- src/main/java/org/POO_AC2/Main.java
+++ src/main/java/org/POO_AC2/Main.java
@@ -21,7 +21,7 @@ public class Main {
         };
         String mensagem = "Por favor, escolha uma opção:";
         String titulo = "Organizacoes Tabajara";
-        ImageIcon icone = new ImageIcon("assets\\menu.jfif");
+        //ImageIcon icone = new ImageIcon("assets\\menu.jfif"); //icone ficou muito grande
         
         JPanel panel = new JPanel(new GridLayout(opcoes.length, 1));
 
@@ -38,7 +38,32 @@ public class Main {
         if (resultado == JOptionPane.OK_OPTION) {
             for (int i = 0; i < opcoes.length; i++) {
                 if (group.getElements().nextElement().isSelected()) {
-                    JOptionPane.showMessageDialog(null, "Você selecionou: " + opcoes[i], titulo, JOptionPane.INFORMATION_MESSAGE, null);
+                    switch(i) {
+                        case 0:
+                            // Código para "Cadastros de Clientes"
+                            break;
+                        case 1:
+                            // Código para "Deletar Cliente pelo CPF ou CNPJ"
+                            break;
+                        case 2:
+                            // Código para "Deletar Cliente pelo nome"
+                            break;
+                        case 3:
+                            // Código para "Cadastro de produtos"
+                            break;
+                        case 4:
+                            // Código para "Efetuação de uma compra"
+                            break;
+                        case 5:
+                            // Código para "Atualização da situação de pagamentos de uma compra"
+                            break;
+                        case 6:
+                            // Código para "Relatórios"
+                            break;
+                        case 7:
+                            // Código para "Sair"
+                            System.exit(0);
+                    }
                     break;
                 }
             }
