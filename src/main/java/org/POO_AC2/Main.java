6a245d3f977cad93d899080745145037ee6ff67f
Tales <tales.a.s.furlan@gmail.com>
2023-11-02 10:38:17 -0300

    menu funcional, em coluna, porém mais feio que o outro e sem a possibilidade de botões

--- src/main/java/org/POO_AC2/Main.java
+++ src/main/java/org/POO_AC2/Main.java
@@ -1,41 +1,14 @@
 package org.POO_AC2;
-
-
-
-
 import java.io.IOException;
-
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import java.awt.GridLayout;
 
-
-
-// 1. Cadastros de Clientes;
-// 2. Deletar cliente pelo CPF ou CNPJ;
-// 3. Deletar cliente pelo nome;
-// 4. Cadastro de Produtos;
-// 5. Efetuação de uma compra;
-// 6. Atualização da situação de pagamento de uma compra;
-// 7. Relatórios;
-// (a) Relação de todos os Clientes que possuem o nome iniciado por uma determinada
-// sequência de caracteres;
-// (b) Relação de todos os Produtos;
-// (c) Busca de um produto pelo nome;
-// (d) Relação de produtos que são perecíveis e que estão com a data de validade vencida;
-// (e) Relação de todas as compras;
-// (f) Busca de uma compra pelo número;
-// (g) Relação de todas as compras que não foram pagas ainda;
-// (h) Relação das 10 últimas compras pagas;
-// (i) Apresentação das informações da compra mais cara;
-// (j) Apresentação das informações da compra mais barata;
-// (k) Relação do valor total de compras feitas em cada mês nos últimos 12 meses.
-// 8. Sair (termina o sistema)
-//JOptionPane
-
-public class Main{
-
+public class Main {
     public static void main(String[] args) throws IOException {
-
         String[] opcoes = {
             "[1] Cadastros de Clientes",
             "[2] Deletar Cliente pelo CPF ou CNPJ", 
@@ -46,14 +19,31 @@ public class Main{
             "[7] Relatorios",
             "[8] Sair"
         };
-        String mensagem = "Por favor, digite o numero do menu para onde deseja navegar.";
+        String mensagem = "Por favor, escolha uma opção:";
         String titulo = "Organizacoes Tabajara";
         ImageIcon icone = new ImageIcon("assets\\menu.jfif");
-        // JOptionPane.showOptionDialog(parentComponent, message, title,
-        //       optionType, messageType, icon, options, initialValue);
-        JOptionPane.showOptionDialog(null, mensagem, titulo,
-        JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE , null, opcoes, null);
+        
+        JPanel panel = new JPanel(new GridLayout(opcoes.length, 1));
 
-
+        ButtonGroup group = new ButtonGroup();
+        
+        for (String opcao : opcoes) {
+            JRadioButton radioButton = new JRadioButton(opcao);
+            panel.add(radioButton);
+            group.add(radioButton);
+        }
+        
+        int resultado = JOptionPane.showConfirmDialog(null, panel, mensagem, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null);
+        
+        if (resultado == JOptionPane.OK_OPTION) {
+            for (int i = 0; i < opcoes.length; i++) {
+                if (group.getElements().nextElement().isSelected()) {
+                    JOptionPane.showMessageDialog(null, "Você selecionou: " + opcoes[i], titulo, JOptionPane.INFORMATION_MESSAGE, null);
+                    break;
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Nenhuma opção selecionada.", titulo, JOptionPane.WARNING_MESSAGE, null);
+        }
     }
 }
